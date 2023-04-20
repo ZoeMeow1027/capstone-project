420c8283f4f502d34b62dabf2b7aadc47062d257
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2023-04-21 01:11:58 +0700

    Changes: Docs and API
    Done API Components:
    - ProductManufacturer
    
    Documents:
    - Changed Database layout.

---
+++ src/api/Migrations/20230420180324_UpdateDb20230421010308.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoneStoreManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDb20230421010308 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowInPage",
+                table: "ProductManufacturers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowInPage",
+                table: "ProductManufacturers");
+        }
+    }
+}
--- src/api/Services/ProductManufacturerService.cs
+++ src/api/Services/ProductManufacturerService.cs
@@ -4,39 +4,102 @@ namespace PhoneStoreManager.Services
 {
     public class ProductManufacturerService : IProductManufacturerService
     {
+        private readonly DataContext _context;
+
+        public ProductManufacturerService(DataContext context)
+        {
+            this._context = context;
+        }
+
         public bool AddProductManufacturer(ProductManufacturer item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.ProductManufacturers.Add(item);
+                return _context.SaveChanges() == 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
         }
 
-        public bool EditProductManufacturer(ProductManufacturer item)
+        public bool UpdateProductManufacturer(ProductManufacturer item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var data = _context.ProductManufacturers.Where(p => p.ID == item.ID).FirstOrDefault();
+                if (data != null)
+                {
+                    data.Name = item.Name;
+                    data.ShowInPage = item.ShowInPage;
+
+                    _context.ProductManufacturers.Update(data);
+                    int numAffected = _context.SaveChanges();
+                    return numAffected == 1;
+                }
+                else
+                {
+                    throw new Exception(string.Format("ProductManufacturer with ID {0} is not exist!", item.ID));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
         }
 
-        public List<ProductManufacturer> FindAllProductManufacturersByName(string name)
+        public List<ProductManufacturer> FindAllProductManufacturersByName(string name, bool includeHidden = false)
         {
-            throw new NotImplementedException();
+            return _context.ProductManufacturers.Where(p =>
+                // Include hidden. This is ignored by default.
+                (includeHidden ? true : p.ShowInPage == true) &&
+                // Filter by name
+                p.Name.ToLower().Contains(name.ToLower())
+            ).ToList();
         }
 
-        public List<ProductManufacturer> GetAllProductManufacturers()
+        public List<ProductManufacturer> GetAllProductManufacturers(bool includeHidden = false)
         {
-            throw new NotImplementedException();
+            // Include hidden. This is ignored by default.
+            return _context.ProductManufacturers.Where(p => (includeHidden ? true : p.ShowInPage == true)).ToList();
         }
 
-        public ProductManufacturer GetProductManufacturerById(int id)
+        public ProductManufacturer? GetProductManufacturerById(int id)
         {
-            throw new NotImplementedException();
+            return _context.ProductManufacturers.FirstOrDefault(p => p.ID == id);
         }
 
         public bool HideProductManufacturer(ProductManufacturer item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var data = _context.ProductManufacturers.Where(p => p.ID == item.ID).FirstOrDefault();
+                if (data != null)
+                {
+                    data.ShowInPage = false;
+                    _context.ProductManufacturers.Update(data);
+                    int numAffected = _context.SaveChanges();
+                    return numAffected == 1;
+                }
+                else
+                {
+                    throw new Exception(string.Format("ProductManufacturer with ID {0} is not exist!", item.ID));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
         }
 
         public bool HideProductManufacturerById(int id)
         {
-            throw new NotImplementedException();
+            var data = _context.ProductManufacturers.Where(p => p.ID == id).FirstOrDefault();
+            return data == null ? false : HideProductManufacturer(data);
         }
     }
 }
