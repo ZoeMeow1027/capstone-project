39b6afcedc00a46d1bb06a23a021368742e10744
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2023-04-24 22:17:04 +0700

    Updated: API and Docs
    API Components:
    - [Added] Post requests (action Add)
    
    Documents:
    - Updated README.md

--- src/api/Controllers/ProductController.cs
+++ src/api/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using PhoneStoreManager.Model;
 using PhoneStoreManager.Services;
 
@@ -65,6 +67,88 @@ namespace PhoneStoreManager.Controllers
             }
 
             return StatusCode(result.StatusCode, result.ToDynamicObject());
+        }
+
+        [HttpPost]
+        public ActionResult Post(JToken data)
+        {
+            try
+            {
+                string? action = (string?)data["action"];
+                string? type = (string?)data["type"];
+                dynamic? data1 = data["data"];
+
+                object dataTemp = null;
+
+                if (action == null || type == null || data1 == null)
+                {
+                    return StatusCode(400, "Invalid requests!");
+                }
+
+                switch (type)
+                {
+                    case "manufacturer":
+                        switch (action)
+                        {
+                            case "add":
+                                if (data1["name"] == null)
+                                {
+                                    return StatusCode(400, "Missing necessary data!");
+                                }
+
+                                productManufacturerService.AddProductManufacturer(new ProductManufacturer()
+                                {
+                                    Name = (string)data1["name"]
+                                });
+                                break;
+                        }
+                        break;
+                    case "category":
+                        switch (action)
+                        {
+                            case "add":
+                                if (data1["name"] == null)
+                                {
+                                    return StatusCode(400, "Missing necessary data!");
+                                }
+
+                                productCategoryService.AddProductCategory(new ProductCategory()
+                                {
+                                    Name = (string)data1["name"]
+                                });
+                                break;
+                        }
+                        break;
+                    case "product":
+                        switch (action)
+                        {
+                            case "add":
+                                if (data1["name"] == null || data1["categoryid"] == null || data1["manufacturerid"] == null || data1["price"] == null)
+                                {
+                                    return StatusCode(400, "Missing necessary data!");
+                                }
+
+                                productService.AddProduct(new Product()
+                                {
+                                    Name = (string)data1["name"],
+                                    CategoryID = (int)data1["categoryid"],
+                                    ManufacturerID = (int)data1["manufacturerid"],
+                                    InventoryCount = (int?)data1["inventorycount"] ?? 0,
+                                    WarrantyMonth = (int?)data1["warrantymonth"] ?? 12,
+                                    Price = (long)data1["price"],
+                                    ShowInPage = (bool?)data1["showinpage"] ?? true
+                                });
+                                break;
+                        }
+                        break;
+                }
+
+                return StatusCode(200, JsonConvert.DeserializeObject<JToken>(data.ToString()));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ReturnResultTemplate(500, ex.Message));
+            }
         }
     }
 }
