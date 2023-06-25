806405adef92114a7fbb60d95d3d35e219cf1af1
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2023-06-25 14:27:59 +0700

    Critical hotfix: For deploy to server

--- src/web/src/main/java/io/zoemeow/pbl6/phonestoremanager/PhoneStoreManagerApp.java
+++ src/web/src/main/java/io/zoemeow/pbl6/phonestoremanager/PhoneStoreManagerApp.java
@@ -5,11 +5,18 @@ import java.security.NoSuchAlgorithmException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import io.zoemeow.pbl6.phonestoremanager.utils.SSLOptions;
 
 @SpringBootApplication
-public class PhoneStoreManagerApp {
+public class PhoneStoreManagerApp extends SpringBootServletInitializer {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PhoneStoreManagerApp.class);
+    }
+
 	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException {
 		SSLOptions.turnOffSslChecking();
 		SpringApplication.run(PhoneStoreManagerApp.class, args);
