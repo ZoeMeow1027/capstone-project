23608c6b05a7763a223e591b6e76bf4ce89ec6b7
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2023-07-02 12:29:25 +0700

    Updated project with small changes

--- src/sql/Database_Select.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM ImageMetadata
-SELECT * FROM [User]
-SELECT * FROM UserSession
-SELECT * FROM ProductManufacturer
-SELECT * FROM ProductCategory
-SELECT * FROM Product
-GO
-DELETE FROM ImageMetadata
